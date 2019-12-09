f7750e5b2eaaa893f816a9efb4458cc77f7bab8e
Henr123466 <57124865+Henr123466@users.noreply.github.com>
2019-12-08 21:50:04 -0600

    Add files via upload

---
+++ Juego/src/clases/Item.java
@@ -0,0 +1,43 @@
+package clases;
+
+import importar.Juego;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.Rectangle;
+
+public class Item {
+     private boolean capturado;
+     private int x;
+ 	 private int y;
+     //private int ancho;
+ 	 //private int alto;
+ 	 private String indiceImagen;
+	
+	
+ 	public Item(int x, int y, int ancho, int alto, String indiceImagen) {
+		super();
+		this.x = x;
+		this.y = y;
+		//this.ancho = ancho;
+		//this.alto = alto;
+		this.indiceImagen = indiceImagen;
+	}
+
+	public void pintar(GraphicsContext graficos) {
+		if (!capturado)
+			graficos.drawImage(Juego.imagenes.get(indiceImagen), this.x, this.y);
+		
+	}
+	
+	public Rectangle obtenerRectangulo() {
+		return new Rectangle(this.x, this.y, 18, 18);
+	}
+
+
+	public boolean isCapturado() {
+		return capturado;
+	}
+
+	public void setCapturado(boolean capturado) {
+		this.capturado = capturado;
+	}
+}
